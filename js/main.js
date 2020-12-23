d50aaabd9367a71222ce32b81fdc168018f43f4f
Swaraj Patil <swarajpatil1703@gmail.com>
2020-12-23 21:38:18 +0530

    Parallax Effect added
    Also added skills bar effect

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    //Navbar effects on Scroll
+    // ========================= Navbar effects on Scroll =============================
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass('sticky');
@@ -14,11 +14,12 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass('active');
     });
 
-    //Typing animation
+    // ================================== Typing animation ==============================
+
     var typed = new Typed(".typing", {
         strings: ["a Graphic Designer", "a Web Developer", "an App Developer"],
-        typeSpeed: 50,
-        backSpeed: 30,
+        typeSpeed: 100,
+        backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
@@ -28,16 +29,28 @@ $(document).ready(function(){
         loop: true
     });
 
+    // ================================== Skills Bar filling animation =======================================
 
-
-
-
-
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if(scroll >= 1800){
+        $('.innerDiv.html').css('width', '90%');
+        $('.innerDiv.css').css('width', '60%');
+        $('.innerDiv.js').css('width', '75%');
+        $('.innerDiv.ps').css('width', '85%');
+        $('.innerDiv.ai').css('width', '80%');
+    }else if(scroll <= 1400){
+        $('.innerDiv').css('width', '0%');
+    }
+});
 
 
 });
 
-// Image Carousel
+
+
+// ================================ Image Carousel ==================================
+
 var slides = document.querySelectorAll('.slide');
 var btns = document.querySelectorAll('.btn');
 let currentSlide = 1;
@@ -89,3 +102,36 @@ var repeat = function(activeClass){
     repeater();
 }
 repeat();
+
+
+// ===================================== Parallax Effect =====================================
+/*
+(function(){
+    document.addEventListener('mousemove', parallax);
+    const elem = document.querySelector('#parallax');
+
+    function parallax(e){
+        let w = window.innerWidth / 2;
+        let h = window.innerHeight / 2;
+        let mouseX = e.clientX;
+        let mouseY = e.clientY;
+        let depth1 = `${50-(mouseX-w)*0.01}% ${50-(mouseY-h)*0.01}%`;
+        let depth2 = `${50-(mouseX-w)*0.02}% ${50-(mouseY-h)*0.02}%`;
+        let depth3 = `${50-(mouseX-w)*0.01}% ${50-(mouseY-h)*0.01}%`;
+        let x = `${depth3},${depth2},${depth1}`;
+        elem.style.backgroundPosition = x;
+    }
+})();
+*/
+document.addEventListener('mousemove', parallax);
+
+function parallax(e){
+    this.querySelectorAll('.layer').forEach(layer => {
+        const speed = layer.getAttribute('data-speed');
+
+        const x = (window.innerWidth - e.pageX*speed)/100;
+        const y = (window.innerHeight - e.pageY*speed)/100;
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    })
+}
